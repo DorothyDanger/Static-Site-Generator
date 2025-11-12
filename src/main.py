8e13ddae108ed233192131fd9c946b36bf0929d4
DorothyDanger <harriotpower@hotmail.com>
2025-11-12 06:19:33 +0000

    Tested using main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from textnode import TextNode, TextType
+from functions import *
 
 def main():
     text1 = TextNode("Hello, World!", TextType.ITALIC, None)
 
     print(text1)
+    copy_static_to_public()
 
 
 if __name__ == "__main__":
